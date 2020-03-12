d3bc1d13fa5e07fcead84728a796a2a8237ac1a3
Sav22999 <saverio.morelli@outlook.it>
2020-03-13 00:55:37 +0100

    0.2.6a
    - Fix #32

--- app/src/main/java/org/commonvoice/saverio/RestartActivity.kt
+++ app/src/main/java/org/commonvoice/saverio/RestartActivity.kt
@@ -4,6 +4,8 @@ import android.annotation.TargetApi
 import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
+import android.content.res.Configuration
+import android.content.res.Resources
 import android.os.Build
 import android.os.Bundle
 import android.os.LocaleList
