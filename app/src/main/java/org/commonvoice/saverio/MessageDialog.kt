618cf78eb72a2c2ee7bd28620a82ad5d62dfc51b
MrIndeciso <mrindeciso@libero.it>
2020-04-16 13:39:44 +0200

    -Simplified code inside MessageDialog by using a Primary Constructor

--- app/src/main/java/org/commonvoice/saverio/MessageDialog.kt
+++ app/src/main/java/org/commonvoice/saverio/MessageDialog.kt
@@ -7,62 +7,47 @@ import androidx.appcompat.app.AlertDialog
 import androidx.core.view.isGone
 import kotlinx.android.synthetic.main.message_dialog.view.*
 
-class MessageDialog {
-    private var message_type: Int = 0 // 0->standard (Ok)
-    private var message_text: String = ""
-    private var message_title: String = ""
-    private var message_details: String = ""
-    private var context: Context? = null;
+class MessageDialog(
+    private val context: Context,
+    private val message_type: Int,
+    private val message_title: String,
+    private val message_text: String,
+    private val details: String = "") {
 
-    constructor(context: Context, title: String, text: String) {
-        this.context = context
-        message_type = 0
-        this.message_title = title
-        this.message_text = text
-    }
-
-    constructor(context: Context, type: Int, title: String, text: String, details: String = "") {
-        this.context = context
-        this.message_type = type
-        this.message_title = title
-        this.message_text = text
-        this.message_details = details
-    }
+    constructor(context: Context, title: String, text: String) : this(context,  0, title, text) // 0->standard (Ok)
 
     fun show() {
-        if (this.context != null) {
-            try {
-                val dialogView =
-                    LayoutInflater.from(this.context).inflate(R.layout.message_dialog, null)
-                val builder = AlertDialog.Builder(this.context!!)
-                    .setView(dialogView)
-                    .setTitle(message_title)
-                //show dialog
-                val alerDialog = builder.show()
-                dialogView.labelTextMessageDialog.setText(this.message_text)
-                if (this.message_details != "") {
-                    dialogView.labelDetailsMessageDialog.setText(this.message_details)
-                    dialogView.btnShowHideDetailsMessageDialog.isGone = false
-                    dialogView.btnShowHideDetailsMessageDialog.paintFlags =
-                        Paint.UNDERLINE_TEXT_FLAG
-                    dialogView.btnShowHideDetailsMessageDialog.setText("Show details")
-                    dialogView.btnShowHideDetailsMessageDialog.setOnClickListener {
-                        if (!dialogView.labelDetailsMessageDialog.isGone) {
-                            dialogView.btnShowHideDetailsMessageDialog.setText("Show details")
-                            dialogView.labelDetailsMessageDialog.isGone = true
-                        } else {
-                            dialogView.btnShowHideDetailsMessageDialog.setText("Hide details")
-                            dialogView.labelDetailsMessageDialog.isGone = false
-                        }
+        try {
+            val dialogView =
+                LayoutInflater.from(this.context).inflate(R.layout.message_dialog, null)
+            val builder = AlertDialog.Builder(this.context)
+                .setView(dialogView)
+                .setTitle(message_title)
+            //show dialog
+            val alerDialog = builder.show()
+            dialogView.labelTextMessageDialog.text = this.message_text
+            if (this.details != "") {
+                dialogView.labelDetailsMessageDialog.text = this.details
+                dialogView.btnShowHideDetailsMessageDialog.isGone = false
+                dialogView.btnShowHideDetailsMessageDialog.paintFlags =
+                    Paint.UNDERLINE_TEXT_FLAG
+                dialogView.btnShowHideDetailsMessageDialog.text = "Show details"
+                dialogView.btnShowHideDetailsMessageDialog.setOnClickListener {
+                    if (!dialogView.labelDetailsMessageDialog.isGone) {
+                        dialogView.btnShowHideDetailsMessageDialog.text = "Show details"
+                        dialogView.labelDetailsMessageDialog.isGone = true
+                    } else {
+                        dialogView.btnShowHideDetailsMessageDialog.text = "Hide details"
+                        dialogView.labelDetailsMessageDialog.isGone = false
                     }
                 }
-                dialogView.btnOkMessageDialog.setOnClickListener {
-                    //dismiss dialog
-                    alerDialog.dismiss()
-                }
-            } catch (exception: Exception) {
-                println("!!-- Exception: MessageDialogActivity MD01 - Details: " + exception.toString() + " --!!")
             }
+            dialogView.btnOkMessageDialog.setOnClickListener {
+                //dismiss dialog
+                alerDialog.dismiss()
+            }
+        } catch (exception: Exception) {
+            println("!!-- Exception: MessageDialogActivity MD01 - Details: " + exception.toString() + " --!!")
         }
     }
 }
