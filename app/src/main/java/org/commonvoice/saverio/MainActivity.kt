c82138a389e63a0890e0b8e3c741881ba156c935
Sav22999 <saverio.morelli@outlook.it>
2020-04-23 10:47:54 +0200

    1.1.1

--- app/src/main/java/org/commonvoice/saverio/MainActivity.kt
+++ app/src/main/java/org/commonvoice/saverio/MainActivity.kt
@@ -26,6 +26,7 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.android.volley.AuthFailureError
+import com.android.volley.BuildConfig
 import com.android.volley.Request
 import com.android.volley.Response
 import com.android.volley.toolbox.JsonObjectRequest
