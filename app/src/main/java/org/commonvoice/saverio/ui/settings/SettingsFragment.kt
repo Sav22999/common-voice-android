4aa22d1038490d6567235a86ec2c9852d7df354e
Sav22999 <saverio.morelli@outlook.it>
2020-08-27 23:00:12 +0200

    Updated ToS link

--- app/src/main/java/org/commonvoice/saverio/ui/settings/SettingsFragment.kt
+++ app/src/main/java/org/commonvoice/saverio/ui/settings/SettingsFragment.kt
@@ -312,7 +312,7 @@ class SettingsFragment : Fragment() {
         val buttonReadCommonVoiceToS: Button = root.findViewById(R.id.buttonReadCommonVoiceToS)
         buttonReadCommonVoiceToS.setOnClickListener {
             val browserIntent =
-                Intent(Intent.ACTION_VIEW, Uri.parse("https://mzl.la/2ZNuj1W"))
+                Intent(Intent.ACTION_VIEW, Uri.parse("https://mzl.la/3b0dN3R"))
             startActivity(browserIntent)
         }
 
