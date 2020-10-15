e55863f8f93e261778d4ef39f3574aa75e80e3f7
MrIndeciso <mrindeciso@libero.it>
2020-10-15 15:55:16 +0200

    Updated libraries
    Signed-off-by: MrIndeciso <mrindeciso@libero.it>

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -1,19 +1,19 @@
 object Versions {
 
-    const val buildGradleVersion = "4.0.1"
+    const val buildGradleVersion = "4.1.0"
     const val kotlinVersion = "1.4.10"
 
-    const val junitVersion = "4.13"
+    const val junitVersion = "4.13.1"
 
     const val appCompatVersion = "1.2.0"
-    const val constraintLayoutVersion = "2.0.1"
-    const val coreKtxVersion = "1.3.1"
+    const val constraintLayoutVersion = "2.0.2"
+    const val coreKtxVersion = "1.3.2"
     const val legacySupportVersion = "1.0.0"
 
     const val workVersion = "2.4.0"
     const val lifeCycleVersion = "2.2.0"
     const val roomVersion = "2.2.5"
-    const val navigationVersion = "2.3.0"
+    const val navigationVersion = "2.3.1"
 
     const val testVersion = "1.3.0"
     const val espressoVersion = "3.3.0"
@@ -28,7 +28,7 @@ object Versions {
 
     // Square
     const val retrofitVersion = "2.9.0"
-    const val moshiVersion = "1.10.0"
+    const val moshiVersion = "1.11.0"
     const val okhttp3Version = "4.9.0"
 
     // Koin
