040292bb05b4022a0d92aa0cf2374d5c3abf63a9
MrIndeciso <mrindeciso@libero.it>
2020-11-29 12:06:27 +0100

    NewSettingsFragment.kt and ListenSettingsFragment.kt are now fully working.
    Signed-off-by: Roberto <mrindeciso@libero.it>

--- app/src/main/java/org/commonvoice/saverio/ui/settings/NewSettingsFragment.kt
+++ app/src/main/java/org/commonvoice/saverio/ui/settings/NewSettingsFragment.kt
@@ -1,12 +1,29 @@
 package org.commonvoice.saverio.ui.settings
 
+import android.content.Intent
+import android.net.Uri
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import androidx.navigation.fragment.findNavController
+import androidx.work.ExistingWorkPolicy
+import androidx.work.WorkManager
+import org.commonvoice.saverio.BuildConfig
+import org.commonvoice.saverio.MainActivity
 import org.commonvoice.saverio.R
 import org.commonvoice.saverio.databinding.FragmentNewSettingsBinding
 import org.commonvoice.saverio.ui.viewBinding.ViewBoundFragment
 import org.commonvoice.saverio.utils.onClick
+import org.commonvoice.saverio_lib.background.ClipsDownloadWorker
+import org.commonvoice.saverio_lib.background.SentencesDownloadWorker
+import org.commonvoice.saverio_lib.preferences.MainPrefManager
+import org.commonvoice.saverio_lib.viewmodels.DashboardViewModel
+import org.commonvoice.saverio_lib.viewmodels.MainActivityViewModel
+import org.koin.android.ext.android.inject
+import org.koin.androidx.viewmodel.ext.android.sharedViewModel
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class NewSettingsFragment : ViewBoundFragment<FragmentNewSettingsBinding>() {
 
@@ -15,6 +32,15 @@ class NewSettingsFragment : ViewBoundFragment<FragmentNewSettingsBinding>() {
         container: ViewGroup?
     ): FragmentNewSettingsBinding {
         return FragmentNewSettingsBinding.inflate(layoutInflater, container, false)
+    }
+
+    private val mainPrefManager: MainPrefManager by inject()
+    private val mainViewModel by viewModel<MainActivityViewModel>()
+    private val workManager by inject<WorkManager>()
+    private val dashboardViewModel by sharedViewModel<DashboardViewModel>()
+
+    private val languagesListShort by lazy {
+        resources.getStringArray(R.array.languages_short)
     }
 
     override fun onStart() {
@@ -51,6 +77,71 @@ class NewSettingsFragment : ViewBoundFragment<FragmentNewSettingsBinding>() {
 
             settingsSectionOfflineMode.onClick {
                 findNavController().navigate(R.id.offlineModeSettingsFragment)
+            }
+        }
+
+        setupLanguageSpinner()
+
+        binding.textRelease.text = "${BuildConfig.VERSION_NAME} (${BuildConfig.VERSION_CODE})"
+
+        binding.textDevelopedBy.setText(R.string.txt_developed_by)
+    }
+
+    private fun setupButtons() = withBinding {
+        buttonReadGuidelines.setOnClickListener {
+            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("https://mzl.la/2Z5OxEQ")))
+        }
+
+        buttonReadCommonVoiceToS.setOnClickListener {
+            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("https://mzl.la/3b0dN3R")))
+        }
+
+        buttonBuyMeACoffee.setOnClickListener {
+            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("https://bit.ly/3aJnnq7")))
+        }
+    }
+
+    private fun setupLanguageSpinner() {
+        binding.languageList.adapter = ArrayAdapter(
+            requireContext(),
+            android.R.layout.simple_list_item_1,
+            resources.getStringArray(R.array.languages)
+        )
+
+        binding.languageList.setSelection(languagesListShort.indexOf(mainPrefManager.language))
+
+        binding.languageList.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                //
+            }
+
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                val selectedLanguage = languagesListShort[position]
+
+                if (selectedLanguage != mainPrefManager.language) {
+                    mainPrefManager.language = selectedLanguage
+
+                    mainViewModel.clearDB().invokeOnCompletion {
+                        SentencesDownloadWorker.attachOneTimeJobToWorkManager(
+                            workManager,
+                            ExistingWorkPolicy.REPLACE
+                        )
+                        ClipsDownloadWorker.attachOneTimeJobToWorkManager(
+                            workManager,
+                            ExistingWorkPolicy.REPLACE
+                        )
+
+                        mainPrefManager.hasLanguageChanged = true
+
+                        (activity as? MainActivity)?.setLanguageUI("restart")
+                    }
+                    dashboardViewModel.lastStatsUpdate = 0
+                }
             }
         }
     }
--- app/src/main/java/org/commonvoice/saverio/ui/settings/nestedSettings/ListenSettingsFragment.kt
+++ app/src/main/java/org/commonvoice/saverio/ui/settings/nestedSettings/ListenSettingsFragment.kt
@@ -1,9 +1,15 @@
 package org.commonvoice.saverio.ui.settings.nestedSettings
 
+import android.util.DisplayMetrics
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import org.commonvoice.saverio.MessageDialog
+import org.commonvoice.saverio.R
 import org.commonvoice.saverio.databinding.FragmentListenSettingsBinding
 import org.commonvoice.saverio.ui.viewBinding.ViewBoundFragment
+import org.commonvoice.saverio_lib.preferences.ListenPrefManager
+import org.commonvoice.saverio_lib.preferences.SettingsPrefManager
+import org.koin.android.ext.android.inject
 
 class ListenSettingsFragment : ViewBoundFragment<FragmentListenSettingsBinding>() {
 
@@ -12,6 +18,62 @@ class ListenSettingsFragment : ViewBoundFragment<FragmentListenSettingsBinding>(
         container: ViewGroup?
     ): FragmentListenSettingsBinding {
         return FragmentListenSettingsBinding.inflate(layoutInflater, container, false)
+    }
+
+    private val settingsPrefManager by inject<SettingsPrefManager>()
+    private val listenPrefManager by inject<ListenPrefManager>()
+
+    override fun onStart() {
+        super.onStart()
+
+        withBinding {
+            switchAutoPlayClips.setOnCheckedChangeListener { _, isChecked ->
+                listenPrefManager.isAutoPlayClipEnabled = isChecked
+
+                if (settingsPrefManager.showConfirmationMessages) {
+                    showMessageDialog(
+                        "",
+                        if (isChecked) getString(R.string.toast_autoplay_clip_on)
+                        else getString(R.string.toast_autoplay_clip_off)
+                    )
+                }
+            }
+            switchAutoPlayClips.isChecked = listenPrefManager.isAutoPlayClipEnabled
+        }
+    }
+
+    private fun showMessageDialog(
+        title: String,
+        text: String,
+        errorCode: String = "",
+        details: String = "",
+        type: Int = 0
+    ) {
+        val metrics = DisplayMetrics()
+        activity?.windowManager?.defaultDisplay?.getMetrics(metrics)
+        //val width = metrics.widthPixels
+        val height = metrics.heightPixels
+        try {
+            var messageText = text
+            if (errorCode != "") {
+                if (messageText.contains("{{*{{error_code}}*}}")) {
+                    messageText = messageText.replace("{{*{{error_code}}*}}", errorCode)
+                } else {
+                    messageText = messageText + "\n\n[Message Code: EX-" + errorCode + "]"
+                }
+            }
+            val message = MessageDialog(
+                requireContext(),
+                type,
+                title,
+                messageText,
+                details = details,
+                height = height
+            )
+            message.show()
+        } catch (exception: Exception) {
+            println("!!-- Exception: MainActivity - MESSAGE DIALOG: " + exception.toString() + " --!!")
+        }
     }
 
 }
