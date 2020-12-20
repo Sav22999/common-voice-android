c93523b7b243fd3fff804945bf63e0dea19b68de
Sav22999 <saverio.morelli@outlook.it>
2020-12-20 01:23:08 +0100

    Added button "Show userId which identify me" in Settings > Advanced

--- app/src/main/java/org/commonvoice/saverio/ui/settings/nestedSettings/AdvancedSettingsFragment.kt
+++ app/src/main/java/org/commonvoice/saverio/ui/settings/nestedSettings/AdvancedSettingsFragment.kt
@@ -1,9 +1,15 @@
 package org.commonvoice.saverio.ui.settings.nestedSettings
 
+import android.util.DisplayMetrics
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_advanced_settings.*
+import org.commonvoice.saverio.MessageDialog
 import org.commonvoice.saverio.databinding.FragmentAdvancedSettingsBinding
 import org.commonvoice.saverio.ui.viewBinding.ViewBoundFragment
+import org.commonvoice.saverio_lib.preferences.MainPrefManager
+import org.commonvoice.saverio_lib.preferences.SettingsPrefManager
+import org.koin.android.ext.android.inject
 
 class AdvancedSettingsFragment : ViewBoundFragment<FragmentAdvancedSettingsBinding>() {
 
@@ -14,4 +20,55 @@ class AdvancedSettingsFragment : ViewBoundFragment<FragmentAdvancedSettingsBindi
         return FragmentAdvancedSettingsBinding.inflate(layoutInflater, container, false)
     }
 
+    private val mainPrefManager by inject<MainPrefManager>()
+
+    override fun onStart() {
+        super.onStart()
+
+        withBinding {
+
+        }
+
+        setupButtons()
+    }
+
+    fun setupButtons() {
+        buttonShowStringIdentifyMe.setOnClickListener {
+            showMessageDialog("",mainPrefManager.statsUserId)
+        }
+    }
+
+    private fun showMessageDialog(
+        title: String,
+        text: String,
+        errorCode: String = "",
+        details: String = "",
+        type: Int = 0
+    ) {
+        val metrics = DisplayMetrics()
+        activity?.windowManager?.defaultDisplay?.getMetrics(metrics)
+        //val width = metrics.widthPixels
+        val height = metrics.heightPixels
+        try {
+            var messageText = text
+            if (errorCode != "") {
+                if (messageText.contains("{{*{{error_code}}*}}")) {
+                    messageText = messageText.replace("{{*{{error_code}}*}}", errorCode)
+                } else {
+                    messageText = messageText + "\n\n[Message Code: EX-" + errorCode + "]"
+                }
+            }
+            val message = MessageDialog(
+                requireContext(),
+                type,
+                title,
+                messageText,
+                details = details,
+                height = height
+            )
+            message.show()
+        } catch (exception: Exception) {
+            println("!!-- Exception: MainActivity - MESSAGE DIALOG: " + exception.toString() + " --!!")
+        }
+    }
 }
