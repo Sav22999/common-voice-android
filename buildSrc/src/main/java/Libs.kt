8da284dbb876c8d47293574b7c529417be742a0d
Roberto <mrindeciso@libero.it>
2021-02-16 15:54:07 +0100

    Fixed non-resolvable dependency

--- buildSrc/src/main/java/Libs.kt
+++ buildSrc/src/main/java/Libs.kt
@@ -29,14 +29,10 @@ object Libs {
     // Lifecycle
     const val viewModel =
         "androidx.lifecycle:lifecycle-viewmodel-ktx:${Versions.lifeCycleVersion}"
-    const val extensions =
-        "androidx.lifecycle:lifecycle-extensions:${Versions.lifeCycleVersion}"
     const val liveData =
         "androidx.lifecycle:lifecycle-livedata-ktx:${Versions.lifeCycleVersion}"
     const val commonJava8 =
         "androidx.lifecycle:lifecycle-common-java8:${Versions.lifeCycleVersion}"
-    const val compiler =
-        "androidx.lifecycle:lifecycle-compiler:${Versions.lifeCycleVersion}"
     const val viewModelSavedState =
         "androidx.lifecycle:lifecycle-viewmodel-savedstate:${Versions.lifeCycleVersion}"
 
