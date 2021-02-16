4d9fb49569f0e9ac6b0d7c95f2263e6ba018f2ae
Roberto <mrindeciso@libero.it>
2021-02-16 15:48:34 +0100

    Updated various libraries

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -1,19 +1,19 @@
 object Versions {
 
     const val buildGradleVersion = "4.1.2"
-    const val kotlinVersion = "1.4.21"
+    const val kotlinVersion = "1.4.30"
 
-    const val junitVersion = "4.13.1"
+    const val junitVersion = "4.13.2"
 
     const val appCompatVersion = "1.2.0"
     const val constraintLayoutVersion = "2.0.4"
     const val coreKtxVersion = "1.3.2"
     const val legacySupportVersion = "1.0.0"
 
-    const val workVersion = "2.4.0"
-    const val lifeCycleVersion = "2.2.0"
+    const val workVersion = "2.5.0"
+    const val lifeCycleVersion = "2.3.0"
     const val roomVersion = "2.2.6"
-    const val navigationVersion = "2.3.2"
+    const val navigationVersion = "2.3.3"
     const val startupVersion = "1.0.0-beta01"
 
     const val testVersion = "1.3.0"
@@ -21,7 +21,7 @@ object Versions {
     const val junitExtVersion = "1.1.2"
 
     // Google
-    const val materialVersion = "1.2.1"
+    const val materialVersion = "1.3.0"
 
     // Coroutines
     const val androidVersion = "1.4.2"
