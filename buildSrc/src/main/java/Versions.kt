9243efe19552ace98460a8e74b6cff9847ae547e
Roberto Bertolini <robertobertolini37@gmail.com>
2021-07-03 17:31:36 +0200

    Updated various outdated libraries
    Signed-off-by: Roberto Bertolini <robertobertolini37@gmail.com>

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -1,35 +1,35 @@
 object Versions {
 
-    const val buildGradleVersion = "4.1.3"
-    const val kotlinVersion = "1.4.31"
+    const val buildGradleVersion = "4.2.1"
+    const val kotlinVersion = "1.5.20"
 
     const val junitVersion = "4.13.2"
 
-    const val appCompatVersion = "1.2.0"
+    const val appCompatVersion = "1.3.0"
     const val constraintLayoutVersion = "2.0.4"
-    const val coreKtxVersion = "1.3.2"
+    const val coreKtxVersion = "1.6.0"
     const val legacySupportVersion = "1.0.0"
 
     const val workVersion = "2.5.0"
-    const val lifeCycleVersion = "2.3.0"
-    const val roomVersion = "2.2.6"
-    const val navigationVersion = "2.3.3"
+    const val lifeCycleVersion = "2.3.1"
+    const val roomVersion = "2.3.0"
+    const val navigationVersion = "2.3.5"
     const val startupVersion = "1.0.0-beta01"
 
     const val testVersion = "1.3.0"
-    const val espressoVersion = "3.3.0"
-    const val junitExtVersion = "1.1.2"
+    const val espressoVersion = "3.4.0"
+    const val junitExtVersion = "1.1.3"
 
     // Google
-    const val materialVersion = "1.3.0"
-    const val gpsAdsLiteVersion = "19.7.0"
+    const val materialVersion = "1.4.0"
+    const val gpsAdsLiteVersion = "20.2.0"
 
     // Coroutines
-    const val androidVersion = "1.4.2"
+    const val androidVersion = "1.5.0"
 
     // Square
     const val retrofitVersion = "2.9.0"
-    const val moshiVersion = "1.11.0"
+    const val moshiVersion = "1.12.0"
     const val okhttp3Version = "4.9.0"
 
     // Koin
